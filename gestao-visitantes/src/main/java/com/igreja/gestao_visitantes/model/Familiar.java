3d267df90edfbd1c58502e577c0ec214b768003b
gio448 <giovannalethicia385@gmail.com>
2026-05-07 20:53:41 -0300

    feat: detalha informações na entidade Familiar

--- gestao-visitantes/src/main/java/com/igreja/gestao_visitantes/model/Familiar.java
+++ gestao-visitantes/src/main/java/com/igreja/gestao_visitantes/model/Familiar.java
@@ -3,6 +3,8 @@ package com.igreja.gestao_visitantes.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Entity
 @Data
 public class Familiar {
@@ -13,4 +15,7 @@ public class Familiar {
 
     private String nome;
     private String parentesco;
+    private boolean evangelico;
+    private String nomeIgreja;
+    private LocalDate dataVisita;
 }
